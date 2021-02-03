5a230a999cf939ba0b14c04c06388a8e7c425f4b
Vadim Frolov <www.kivinet@mail.ru>
2021-02-03 16:57:44 +0300

    Изменил параметры возврата  и название параметра

--- js/main.js
+++ js/main.js
@@ -1,32 +1,3 @@
-/* const getRandomNumberGenerated = function (min, max) {
-  // случайное число от min до (max+1)
-  const rand = min + Math.random() * (max + 1 - min);
-
-  if (min >= max || min < 0 || max < 0) {
-
-    throw new Error('Invalid Variable value');
-  }
-  return Math.floor(rand);
-}
-
-// eslint-disable-next-line no-console
-console.log(getRandomNumberGenerated(0, 1)); */
-
-
-/* const getRandomIntNumber = (min, max, symbolNumber = 1) => {
-  const rand = getRandomNumberGenerated(min, max);
-
-  if (min >= max || min < 0 || max < 0) {
-    // eslint-disable-next-line no-console
-    console.log('error');
-    return;
-  }
-
-  return rand.toFixed(symbolNumber);
-}
-
-getRandomIntNumber(10, 20, 3); */
-
 const getRandomIntInclusive = function (min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
@@ -41,13 +12,12 @@ const getRandomIntInclusive = function (min, max) {
 getRandomIntInclusive(0, 2)
 
 
-const getRandomMapCoordination = function (min, max, coordinate = 1) {
+const getRandomMapCoordination = function (min, max, numberOfDecimal = 2) {
   if (max > min && min >= 0) {
-    return ((Math.random() * (max - min) + min).toFixed(coordinate));
+    return +((Math.random() * (max - min) + min).toFixed(numberOfDecimal));
   }
 
   throw new Error('Invalid Variable value');
 }
 
 getRandomMapCoordination(0, 10);
-
